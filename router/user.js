9a2eccb78082203fa129d81afa755c1d6c7ec881
Kdddru <125441996+Kdddru@users.noreply.github.com>
2024-03-15 21:45:04 +0900

    DB 연결

---
+++ router/user.js
@@ -0,0 +1,13 @@
+import express from 'express'
+
+
+const router = express.Router();
+
+
+router.get('/:id',(req, res)=>{
+  
+  res.send(req.params.id);
+})
+
+
+export default router
--- server.js
+++ server.js
@@ -1,22 +1,40 @@
 import express from 'express'
-
-
-
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+import userRouter from './router/user.js'
 
 const app = express();
 
+dotenv.config();
+
+const uri = process.env.DB_URI;
 const port = process.env.PORT || 3000
 
 
 app.use(express.static('router'));
+app.use(express.json());
 
+
+const myLogger = (req, res, next) =>{
+  console.log(req.url);
+  next();
+}
+
+app.use(myLogger)
+
+
+// home
 app.get('/' , (req , res) => {
   res.send('welcome to server')
 })
 
+// user
+app.use('/user', userRouter)
 
 
-
+mongoose.connect(uri)
+.then(()=>console.log('connect DB'))
+.catch((error)=>console.log(error))
 
 app.listen(port,()=>{
   console.log('server is run')
